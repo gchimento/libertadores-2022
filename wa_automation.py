6f5b79c8113f50f3135257cdf939d4c2919b25c2
Gonzalo Chimento <gchimento@frba.utn.edu.ar>
2024-07-26 20:46:07 -0300

    Update wa_automation.py

--- wa_automation.py
+++ wa_automation.py
@@ -1,20 +1,20 @@
 # import pywhatkit
 
-# pywhatkit.sendwhatmsg_to_group('1KHMLgAL7zB6X9KE4vkPpR', 'hola', 17, 26, 15, False, 3)
+# pywhatkit.sendwhatmsg_to_group('XXXXXXX', 'hola', 17, 26, 15, False, 3)
 
 import time
 from twilio.rest import Client
 
 def river_campeon():
 # while 1:
-    account_sid = 'AC67e7dec23220cca71376a1d3707a8cb7'
-    auth_token = '18592d578b040a3d769861a9078bfba7'
+    account_sid = 'XXXXXXX'
+    auth_token = 'XXXXXXX'
     client = Client(account_sid, auth_token)
 
     message = client.messages.create(
         from_='whatsapp:+14155238886',
         body='Felicitaciones al Club Atlético River Plate por la obtención del título de *campeón* de la Copa Libertadores de América 2022. Marcelo Gallardo es el mejor director técnico de América y Julián Álvarez no va a sufrir lesiones que condicionen su presencia como titular en futuros partidos a disputar.',
-        to='whatsapp:+5491159351105'
+        to='whatsapp:+XXXXXXX'
     )
 
     print(message.sid)
